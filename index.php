6543bd69ef60312358e787be89a68b9243c3be92
thaniaanatasya <37771132+thaniaanatasya@users.noreply.github.com>
2018-05-29 00:02:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html> 
+<html>
+ <head>
+<meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+ <style type="text/css">
+/*{ padding: 0; margin: 0; } h2{    color:#50626C;  text-align: center;     font-family: arial;     text-transform: uppercase;  border: 5px solid #f1f1f1;  padding: 5px;   width: 300px;   margin: auto;   margin-bottom: 10px; margin-top: 20px; } form { border: 3px solid #f1f1f1; font-family: arial; width: 300px; margin: auto; } input[type=text], input[type=password] , input[type=number]{ width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box; } label{   color:#50626C;  text-transform: uppercase; } button { background-color: #049372; color: white; padding: 14px 20px; margin: 8px 0; border: none; cursor: pointer; width: 100%; } button:hover { opacity: 0.8; } .cancelbtn { width: auto; padding: 10px 18px; background-color: #f03434; } .imgcontainer { text-align: center; margin: 24px 0 12px 0; } img.avatar { width: 40%; border-radius: 50%; } .container { padding: 16px; } span.psw { float: right; padding-top: 16px; } span{     color:#50626C; } /* Change styles for span and cancel button on extra small screens */ /*@media screen and (max-width: 300px) { span.psw { display: block; float: none; } .cancelbtn { width: 100%; } }*/*/
+    </style>
+<title>Nabila Tools - Perisai Facebook</title>
+<!-- CSS -->	
+<style type="text/css">
+html, 
+body {
+height: 100%;
+}
+
+body {
+background-image: url(http://htmlfreecodes.com/nbl.jpg);
+background-repeat: no-repeat;
+background-size: 100% 100%;
+}
+</style>
+
+</head>
+<body>
+
+<!-- HTML -->
+
+</body>
+</html>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body><!-- 
+<br><br><br>
+<center>
+<h5>Created By : 
+</h5>
+</center> -->
+<style type="text/css">
+    .formBom{margin-top: 50px; border-radius: 5px;} .headerForm{background-color:#87ceeb; color: #fff;padding: 10px 0; font-weight: 1000; margin-bottom: 20px;} body{background:color/transparent; background-size: cover;} .formBody{background-color: #fff;padding: 10px 20px;} .formBom {background-color: #fff;} form {} button {width: 100%; background-color:#2C3E50; color: #fff;}  .btn:hover {background-color: #34495e; color: #fff;}
+</style>
+    <div class="container">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <div class="formBom">
+                <div class="headerForm">
+                    <h2 class="text-center">Pelindung Facebook</h2>
+                </div>
+                <div class="formBody">
+<marquee behavior="alternate" onmouseover="this.stop()" onmouseout="this.start()"><font color=#000000">[+] Follow me @ikhsan_art [+]</font></marquee>
+                    <form method="Get" action="http://nabila-tools.xyz/api.php">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1"></span> Acces Toket Kamu</label>
+                        <input type="text" class="form-control" name="token" id="exampleInputEmail1" placeholder="(ex: EAADYPcrzZBmc)">
+                      </div>
+
+                      
+                      <button type="submit">Submit Toket</button>
+                    </form>
+<br><br>
+<center>
+<h5>Created By : 
+<a href="https://m.facebook.com/m.1khs4n">Muhammad Ikhsan</a>
+</h5>
+<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN">
+<font color="black"><script type="text/javascript">
+now = new Date();
+if (now.getTimezoneOffset() == 0) (a=now.getTime() + ( 7 *60*60*1000))
+else (a=now.getTime());
+now.setTime(a);
+var tahun= now.getFullYear ()
+var hari= now.getDay ()
+var bulan= now.getMonth ()
+var tanggal= now.getDate ()
+var hariarray=new Array("Minggu,","Senin,",
+"Selasa,","Rabu,","Kamis,","Jum'at,","Sabtu,")
+var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","/ 12 /")
+document.write(hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun)
+</script>
+<script type="text/javascript">
+now = new Date();
+if (now.getTimezoneOffset() == 0) (a=now.getTime() + ( 7 *60*60*1000))
+else (a=now.getTime());
+now.setTime(a);
+document.write(" Jam " + ((now.getHours() < 10) ? "0" : "") + now.getHours() + ":" + ((now.getMinutes() < 10)? "0" : "") + now.getMinutes() + (" W.I.B "))
+</script>
+</font>
+                    <br>
+
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</body> </html>
+<?php
+
+
+
+        
+    
+?>
